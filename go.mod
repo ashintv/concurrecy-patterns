473b1a3abff04727c47a27e65e67b31e7aa4ea06
ASHIN T V <146656061+ashintv@users.noreply.github.com>
2025-12-25 21:04:36 +0530

    or-channel

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ashintv/concpatterns
+
+go 1.25.0
---
+++ or-channel/the_or_channel.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+/*
+
+	In many situations we may have to do or operation in a no.of channels
+	for that creating a single or channel that can take n no.of channels a return a single channel
+	the single channels get called (done) whenever any of the input channel get read
+
+*/
+
+func or(channels ...<-chan interface{}) <-chan interface{} {
+
+	switch len(channels) {
+	case 0:
+		return nil
+
+	case 1:
+		return channels[0]
+	}
+
+	orDone := make(chan interface{})
+
+	go func() {
+		defer close(orDone)
+		switch len(channels) {
+		case 2:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			}
+
+		default:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			case <-channels[2]:
+			case <-or(append(channels[3:], orDone)...):
+			}
+		}
+	}()
+	return orDone
+}
+
+func main() {
+	sig := func() <-chan interface{} {
+		c := make(chan interface{})
+
+		go func() {
+			defer close(c)
+			sleep := rand.Intn(5) + 1
+			fmt.Println("Sleeping for", sleep, "seconds")
+			time.Sleep(time.Duration(sleep) * time.Second)
+
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(),
+		sig(),
+		sig(),
+		sig(),
+	)
+	fmt.Println("Done after:", time.Since(start))
+}
