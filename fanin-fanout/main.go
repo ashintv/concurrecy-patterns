6754321cf99abb1acecccf9afcdf740acdd1de1f
ASHIN T V <146656061+ashintv@users.noreply.github.com>
2025-12-27 00:22:25 +0530

    fan-in-out ++

--- fanin-fanout/main.go
+++ fanin-fanout/main.go
@@ -23,24 +23,24 @@ func doWork(done <-chan interface{}) <-chan interface{} {
 	workStream := make(chan interface{})
 
 	go func() {
-		num := rand.Int()
+		num := rand.Intn(5)
+		log.Printf("Routine Write after %ds" , num)
 		defer close(workStream)
-		time.Sleep(time.Second * 1)
-		for {
-			select {
-			case <-done:
-			case workStream <- num:
-			}
+		select {
+		case <-done:
+		case <-time.After(time.Duration(num) * time.Second):
+
+			workStream <- num
 		}
+
 	}()
 	// adding a sleep for simulating work
 	return workStream
 }
 
 func fanout(done <-chan interface{}) []<-chan interface{} {
-	// assuming 10 piece of data()
-	numWorkers := 5
-
+	log.Printf("Spinning 3 ")
+	numWorkers := 3
 	workers := make([]<-chan interface{}, numWorkers)
 	for i := range numWorkers {
 		workers[i] = doWork(done)
@@ -51,7 +51,7 @@ func fanout(done <-chan interface{}) []<-chan interface{} {
 func fanin(done <-chan interface{}, channels []<-chan interface{}) <-chan interface{} {
 	var wg sync.WaitGroup
 	multPlexedStream := make(chan interface{})
-
+	log.Printf("Combing data")
 	multiplexer := func(c <-chan interface{}) {
 		defer wg.Done()
 		for i := range c {
